9bed24a0bb8425e0210d073f1b5a6341e44f5202
исп-31 <-31@LAB25-07>
2024-10-31 11:05:17 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        public static string Gen(int n)
+        {
+            string a = "";
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int x = (rnd.Next(2,14));
+                a += x + " ";
+            }
+            return a;
+        }
+
+        public static string Ras(string r)
+        {
+            string x = "";
+            string z = "";
+            int sum = 0;
+            for (int i = 0; i < r.Length; i++)
+            {
+                if (Char.IsNumber(r[i]))
+                {
+                    x += r[i];
+                }
+                else
+                {
+                    int y = Convert.ToInt32(x);
+                    x = "";
+                    if (y < 8)
+                    {
+                        sum += y;
+                    }
+                }
+            }
+            z = sum.ToString();
+            return z;
+        }
+
+
+    }
+}
---
+++ Практическая№1 Трусов/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace Практическая_1_Трусов
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void gen(object sender, RoutedEventArgs e)
+        {          
+            bool f1;
+            f1 = Int32.TryParse(zn.Text, out int n);
+            if (f1 == true && n > 0)
+            {
+                n = Convert.ToInt32(zn.Text);
+                ch.Text = Class1.Gen(n);
+            }
+            else MessageBox.Show("Введите правильное значение.");
+        }
+
+        private void Ras(object sender, RoutedEventArgs e)
+        {
+            string r = ch.Text;
+            rez.Text = Class1.Ras(r);
+        }
+
+        private void Clean(object sender, RoutedEventArgs e)
+        {
+            rez.Clear();
+            ch.Clear();
+            zn.Clear();
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void about(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сформировать n целых чисел в диапазоне 2 - 14.\nНайти сумму чисел < 8.Результат вывести на экран.");
+        }
+    }
+}
